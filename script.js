5d6eae8264bc22306ce3dac4c29ba21aa36fafa2
inclined4X <jezemiahsam48@gmail.com>
2024-02-15 19:39:03 +0000

    added the randomsquare function for putting mole in random squares

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+// getting the elements
+
 const squares = document.querySelectorAll(".square");
 const mole = document.querySelector(".mole");
 const timeLeft = document.querySelector("#time-left");
-document.querySelector("#score");
+const score = document.querySelector("#score");
+
+let result = 0;
+
+function randomSquare() {
+  //removing the mole for a clean slate
+  squares.forEach((square) => {
+    square.classList.remove("mole");
+  });
+
+  // using a random number to place the mole
+  let randomSquare = squares[Math.floor(Math.random() * 9)];
+  randomSquare.classList.add("mole");
+}
+
+randomSquare();
