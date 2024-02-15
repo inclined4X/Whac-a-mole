95033a1a0f50ced6cf2bc9fb80d1a13244a6ee66
inclined4X <jezemiahsam48@gmail.com>
2024-02-15 19:23:03 +0000

    getting the elements

---
+++ script.js
@@ -0,0 +1,4 @@
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelector(".mole");
+const timeLeft = document.querySelector("#time-left");
+document.querySelector("#score");
